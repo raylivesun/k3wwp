a39382ea46002616e434e689e9a63031be2b9b52
raylivesun <162396999+raylivesun@users.noreply.github.com>
2024-10-03 20:26:22 -0300

    k3wwp
    Business logic language common R to gnu architecture platform emacs IDEs to logical series stream
    formation academic gnu to hackers common lisp offices about situation needs logic of measure level to
    produce the education of master to math and science and physics and chemical and dynamic to dialetic
    about conduction series stream.

---
+++ platform/gnu/test/m3jfm.R
@@ -0,0 +1,19 @@
+#!/usr/bin/r
+
+# static reference verify the elements
+2+3
+
+# call to values static template elements in fractions
+14/6
+
+# verify the fits about recipes
+14/6+5
+
+# read key entry to table sort
+14/(6+5)
+
+# unit special ratter
+3^2
+
+# mic phones static new keep in face
+2^3
